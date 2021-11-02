c8625eee6a10965fb7471d7b0bf31be2b16ad647
wangcheng08 <wangcheng08@bilibili.com>
2021-11-02 15:18:44 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/NorwayLobster/moduletest
+
+go 1.17
+
+require github.com/fyf2173/moduledependencytest v1.0.0
---
+++ hello.go
@@ -0,0 +1,16 @@
+package moduletest
+
+// import "github.com/fyf2173/moduledependencytest"
+// import "github.com/NorwayLobster/gomodone"
+
+func Hello1() string {
+	return "hello world"
+}
+
+// func Hello() string {
+// return moduledependencytest.Hello()
+// }
+
+// func Hello() string {
+// return gomodone.SayHi("world")
+// }
