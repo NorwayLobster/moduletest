da8d61985c8ca5bdbd313b70931dd81345cc9317
wangcheng08 <wangcheng08@bilibili.com>
2021-11-03 13:51:23 +0800

    add Hello2()

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/NorwayLobster/moduletest
 go 1.17
 
 require github.com/NorwayLobster/gomodone v0.0.0-20211102074849-5d322c6e33f6
+
+require github.com/fyf2173/moduledependencytest v1.0.0 // indirect
--- hello.go
+++ hello.go
@@ -1,15 +1,17 @@
 package moduletest
 
-// import "github.com/fyf2173/moduledependencytest"
-import "github.com/NorwayLobster/gomodone"
+import (
+	"github.com/NorwayLobster/gomodone"
+	"github.com/fyf2173/moduledependencytest"
+)
 
 func Hello() string {
 	return "Hello World"
 }
 
-// func Hello() string {
-// return moduledependencytest.Hello()
-// }
+func Hello2() string {
+	return moduledependencytest.Hello()
+}
 
 func Hello1() string {
 	return gomodone.SayHi("World")
