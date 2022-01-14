11afb48d11c9037930abe8ec9cb31833673916f8
wangcheng08 <wangcheng08@bilibili.com>
2022-01-14 18:06:41 +0800

    return and handle an error

--- example/main.go
+++ example/main.go
@@ -2,11 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/NorwayLobster/moduletest/greetings"
 	"github.com/NorwayLobster/moduletest/hello"
 )
 
 func main() {
 	fmt.Println("hello world")
 	hello.Hello()
+
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	// log.SetFlags(0)
+	log.SetFlags(1)
+	log.SetFlags(2)
+	log.SetFlags(3)
+
+	// Request a greeting message.
+	message, err := greetings.Hello("")
+	// message, err := greetings.Hello("xiaowang")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
+	fmt.Println(message)
 }
---
+++ readme.md
@@ -0,0 +1,6 @@
+# module test repo
+
+
+note:
+ 1.  依赖本地module的用法: https://go.dev/doc/tutorial/call-module-code
+	 replace example.com/greetings => ../greetings
